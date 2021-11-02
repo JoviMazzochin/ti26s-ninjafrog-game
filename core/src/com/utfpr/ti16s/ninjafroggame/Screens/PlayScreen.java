d6540cd3bfaebea42f661776a445563a1fa3a7e4
João Victor Costa Mazzochin <joaomazzochin@gmail.com>
2021-11-02 15:12:18 -0300

    feat: creating ground and traps bodies/fixtures

--- core/src/com/utfpr/ti16s/ninjafroggame/Screens/PlayScreen.java
+++ core/src/com/utfpr/ti16s/ninjafroggame/Screens/PlayScreen.java
@@ -5,9 +5,16 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.PolygonMapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
+import com.badlogic.gdx.math.Polygon;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -25,6 +32,10 @@ public class PlayScreen implements Screen {
     private TiledMap map;
     private OrthoCachedTiledMapRenderer renderer;
 
+    //Box2d variables
+    private World world;
+    private Box2DDebugRenderer box2DDebugRenderer;
+
     public PlayScreen(NinjaFrogGame game) {
         this.game = game;
         gameCam = new OrthographicCamera(); // Create cam used to follow NinjaFrog
@@ -39,6 +50,41 @@ public class PlayScreen implements Screen {
         //setting gamecam
         gameCam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
 
+        world = new World(new Vector2(0,0), true);
+        box2DDebugRenderer = new Box2DDebugRenderer();
+
+        BodyDef bdef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fDef = new FixtureDef();
+        Body body;
+
+        //creating ground bodies/fixtures
+        for (MapObject object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            fDef.shape = shape;
+            body.createFixture(fDef);
+        }
+
+        // creating trap bodies/fixtures
+        for (MapObject object : map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            fDef.shape = shape;
+            body.createFixture(fDef);
+        }
     }
 
     public void handleInput(float dt) {
@@ -61,13 +107,18 @@ public class PlayScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        //separate update logic from render
         update(delta);
 
         //Clear screen
         Gdx.gl.glClearColor(1,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        //render game map
         renderer.render();
+
+        //renderer Box2DDegugLines
+        box2DDebugRenderer.render(world, gameCam.combined);
 
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
